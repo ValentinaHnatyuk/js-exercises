88fca8e1ddfe7df63c6ccee931202387b24c51a4
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2022-11-10 14:34:00 +0100

    Add files via upload

---
+++ 05.objects copying/exercise-1.js
@@ -0,0 +1,16 @@
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+};
+
+const person2 = person1;
+
+person2.firstName = "Simon";
+
+/* perche' abbiamo un solo obj ,cio' che viene cambiato e' 
+la variabile che punta allo stesso oggetto, quindi modificando un valore 
+di person1 o person2 in entrambi i casi modifichiamo lo stesso oggetto */
+
+console.log(person1);
+console.log(person2);

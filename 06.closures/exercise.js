254a6ec9256b7ce47cdcd41c140afb7b37e7d6ce
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2022-11-10 14:43:46 +0100

    Update exercise.js

--- 06.closures/exercise.js
+++ 06.closures/exercise.js
@@ -4,12 +4,11 @@ function printName() {
     let helloName = "Hello John";
      
     function inner() {
-        console.log(helloName);
         return helloName;
     }
     return inner;
     
 }
 
-printName()();
+console.log(printName()());
 

adab1af2d003edde0bdc9e09c5f37fbdac40c564
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2022-12-02 18:57:21 +0100

    Add files via upload

---
+++ istanceof operator 59/exercise-6.js
@@ -0,0 +1,37 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(val) {
+    if (val instanceof Square) {
+      return val.side * val.side;
+    } else if (val instanceof Rectangle) {
+      return val.width * val.height;
+    } else if (val instanceof Circle) {
+      return val.radius * val.radius * 3.14;
+    }
+  }
+}
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));

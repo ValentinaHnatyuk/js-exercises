7c9e08e2fa31fedcde3896e668a0d975ef5f9704
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2023-01-26 17:14:38 +0100

    Update exercise-1.js

--- 09.objects method/exercise-1.js
+++ 09.objects method/exercise-1.js
@@ -3,10 +3,6 @@ const person = {
   lastName: 'Rossi',
   age: 25
 }
-
 console.log(Object.keys(person));
-
-
-/* non richiesto dall'esercizio, ma volevo provare ,
- come mai l'output e'  [Array(2), Array(2), Array(2)] ? */ 
-console.log(Object.entries(person));
+console.log(Object.value(person));
+// Print values of person using Object.keys

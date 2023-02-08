d51ba84993cc90845d2865b78043a500a7e23a3a
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2023-02-08 18:25:46 +0100

    Update exercise-3.js

--- 03.arrow functions/exercise-3.js
+++ 03.arrow functions/exercise-3.js
@@ -1,20 +1,15 @@
 
-const sum = (a, b) => {
-  return a + b;
-}
+const sum = (a, b) => a + b;
 
 
-const subtract = (a,b) => {
-  return a - b;
-}
 
-const multiply = (a,b) => {
-  return a * b;
-}
+const subtract = (a,b) => a - b;
 
-const divide = (a,b) => {
-  return a / b;
-}
+
+const multiply = (a,b) => a * b;
+
+const divide = (a,b) => a / b;
+
 
 const log = function (value) {
   console.log(value);

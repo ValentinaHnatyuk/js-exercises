7a4d449209ea22179c18d24189b007dcf76e729a
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2023-02-18 16:25:50 +0100

    Update exercise.js

--- promise 76/exercise.js
+++ promise 76/exercise.js
@@ -1,1 +1,12 @@
 const number = 15;
+const myPromise =new Promise((resolve, reject) => {
+  if (number > 10) {
+    resolve("number is greater than 10!");
+  } else {
+    reject("number is less than 10!");
+  }
+});
+
+myPromise
+ .then((val) => console.log(val))
+ .catch((err) => console.log(err))
